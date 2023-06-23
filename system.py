c7ad80d2498be810fe772a8f2d6f1298283a5c16
tearne <tearne@gmail.com>
2023-06-23 22:02:28 +0100

    Migrate from stuff

---
+++ system.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import util
+import cargo
+
+
+def main(password):
+    install_helix(password)
+    install_zellij()
+    install_broot()
+
+
+def install_zellij():
+    # todo check if already installed
+    cargo.install_cargo()
+    util.run("cargo install --locked zellij")
+    
+def install_broot(password=util.Password()):
+    if util.run("broot -V").returncode == 0:
+        print("broot already installed")
+        return
+
+    cargo.install_cargo()
+    util.sudo("apt install -y build-essential libxcb1-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev", password)
+    util.run("cargo install --locked broot")
+
+
+def install_lazygit(password=util.Password()):
+    ver = util.run(
+        r"""curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*'""",
+        capture_output=True
+    ).stdout.rstrip()
+    arch = ("arm64" if util.is_aarch64 else "x86_64")
+
+    file = f"lazygit_{ver}_Linux_{arch}.gz"
+
+    util.run(f"curl -Lo lazygit.tar.gz \"https://github.com/jesseduffield/lazygit/releases/latest/download/{file}\"")
+    util.run("tar xf lazygit.tar.gz lazygit")
+    util.sudo("install lazygit /usr/local/bin", password)
+
+
+def install_helix(password=util.Password()):
+    util.sudo("add-apt-repository -y ppa:maveonair/helix-editor", password)
+    util.sudo("apt update", password)
+    util.sudo("apt install helix", password)
+
+
+if __name__ == "__main__":
+    main(util.Password())
---
+++ util.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import getpass
+import subprocess
+
+import ensure
+ensure.package("pexpect")
+
+import pexpect
+
+
+class Password:
+    def __init__(self):
+        self.p = None
+
+    def get(self):
+        if self.p:
+            return self.p
+        else:
+            self.p = getpass.getpass()
+            return self.p
+
+
+def run(cmd, cwd=None, capture_output=False):
+    print(" *** Running:", cmd)
+    
+    return subprocess.run(
+        cmd,
+        shell=True,
+        capture_output=capture_output,
+        text=True,
+        encoding='utf8',
+        cwd=cwd
+    )
+
+
+def am_root():
+    return os.geteuid() == 0
+
+
+def is_aarch64():
+    return os.uname().machine == 'aarch64'
+
+
+def sudo(cmd, password, timeout=None, cwd=None):
+    if am_root():
+        run(cmd, cwd)
+    else:
+        command = f"sudo {cmd}"
+        print(" *** Running (sudo): ", cmd)
+        print(f"     Timeout = {timeout}")
+        child = pexpect.spawnu(command, cwd=cwd, timeout=timeout)
+        child.logfile_read = sys.stdout
+        options = ['password', pexpect.TIMEOUT, pexpect.EOF]
+        index = child.expect(options, timeout=1)
+        if index > 0:
+            print(
+                f"Error waiting for password prompt:\
+                {options[index]} - {child.before.decode()}"
+            )
+            sys.exit(1)
+
+        child.sendline(password.get())
+
+        options = [pexpect.EOF, 'try again', pexpect.TIMEOUT]
+        index = child.expect(options,timeout=timeout)
+        if index == 0:
+            return
+        elif index == 1:
+            print(f"Authentication failure: {options[index]}")
+            sys.exit(1)
+        else:
+            print(f"Command failure running: {cmd}\n {options[index]}")
+            sys.exit(1)

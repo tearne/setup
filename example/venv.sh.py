ca36fce50410acf6f38d08a826a5843c651d73c4
tearne <tearne@gmail.com>
2023-10-15 22:47:42 +0100

    Moar terse

--- example/venv.sh.py
+++ example/venv.sh.py
@@ -10,15 +10,11 @@
 # - The heredoc can make line error reporting messy.
 #   An alternative is importing another script from file.
 
-VENV_NAME=venv; PYTHON=python3.11
+VENV=venv; PYTHON=python3
 set -eu
 cd "$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
-if ! test -f $VENV_NAME/bin/python; then
-  echo " * Creating virtual environment: ${VENV_NAME}"
-  $PYTHON -m venv $VENV_NAME
-fi
-. $VENV_NAME/bin/activate
-$PYTHON - $@ <<END_PYTHON
+if ! test -f $VENV/bin/python; then echo " * Creating ${VENV}" && $PYTHON -m venv $VENV; fi
+. $VENV/bin/activate && $PYTHON - $@ <<END_PYTHON
 import importlib, subprocess, sys
 def ensure(package):
   try: importlib.import_module(package)
@@ -32,8 +28,21 @@ ensure("rich")
 ###########################
 # Python user script start
 ###########################
+from rich.console import Console
+console = Console()
+console.print("\n[bold]Languages\n")
 
-from rich import print
-print("[bold green]Hello[/bold green], [bold blue]World[/bold blue]!", ":globe_showing_europe-africa:")
+console.rule('Python', style='blue')
+console.print('''Python is a general-purpose, dynamic, object-oriented \
+programming language. The design purpose of the Python language \
+emphasizes programmer productivity and code readability.''')
+console.print()
+
+console.rule('Rust', style='red')
+console.print('''Rust is a multi-paradigm, general-purpose programming \
+language that emphasizes performance, type safety, and concurrency with \
+features including memory ownership model and zero-cost abstractions.''')
+console.print()
+
 
 END_PYTHON

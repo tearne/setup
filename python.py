0c2f09a00c5207c58b285b2bc4f795d1bf619409
tearne <tearne@gmail.com>
2023-12-20 22:13:20 +0000

    Setting up upi50

--- python.py
+++ python.py
@@ -4,40 +4,42 @@ import util
 
 
 def main(password):
-    py_version = "3.11.5"
+    py_version = "3.12.1"
     compile_and_install_python(py_version, password)
-    ensure_venv(
-        py_version,
-        "GitPython",
-        "boto3",
-        "dpath",
-        "lxml",
-        "polars",
-        "seaborn",
-        "pyarrow",
-        "pandas"
-    )
+    # ensure_venv(
+    #     py_version,
+    #     "GitPython",
+    #     "boto3",
+    #     "dpath",
+    #     "lxml",
+    #     "polars",
+    #     "seaborn",
+    #     "pyarrow",
+    #     "pandas"
+    # )
 
 
 def ensure_venv(version, *libs):
     ver_minor = '.'.join(version.split('.')[0:2])
     util.run(f"python{ver_minor} -m venv ~/venv")
-    util.run(f""". ~/venv/bin/activate && pip{ver_minor} install {" ".join(libs)}""")
+    util.run(f""". ~/venv/bin/activate \
+             && pip{ver_minor} install {" ".join(libs)}""")
 
 
-def compile_and_install_python(version, password, del_tmp=True):
-    ver_minor = '.'.join(version.split('.')[0:2])
+def compile_and_install_python(ver, password, del_tmp=True):
+    ver_minor = '.'.join(ver.split('.')[0:2])
 
     if util.run(f"python{ver_minor} --version").returncode == 0:
         print(" - already installed")
         return
-    
     util.sudo("apt update", password, 60)
     util.sudo("""apt install -y curl wget python3-pip""", password, 60)
 
-
     util.sudo(
-        "DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev",
+        "DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install \
+        build-essential libreadline-dev libncursesw5-dev libssl-dev \
+        libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev \
+        libffi-dev zlib1g-dev",
         password,
         60
     )
@@ -46,11 +48,13 @@ def compile_and_install_python(version, password, del_tmp=True):
     print("* Build in ", tempdir)
 
     util.run(
-        f"wget -c https://www.python.org/ftp/python/{version}/Python-{version}.tar.xz  -O - | tar -Jx",
+        f"wget \
+        -c https://www.python.org/ftp/python/{ver}/Python-{ver}.tar.xz \
+        -O - | tar -Jx",
         tempdir
     )
 
-    py_dir = f"{tempdir}/Python-{version}"
+    py_dir = f"{tempdir}/Python-{ver}"
     util.sudo(
         "./configure --enable-optimizations",
         password,
